4ba45ce51c764672cf0ad6014f74675f40dc5f57
Wayne Myo <waiyankm93@gmail.com>
2022-08-20 17:46:25 +0900

    flow-builder WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "../src/style.css"
+import OverviewFlow from "./Flow-Builder/FlowBuilder";
+import styled from "styled-components";
+import { Container, Row, Col } from "react-bootstrap";
 
-function App() {
+const ContainerDiv = styled(Container)`
+  font-family: sans-serif;
+  text-align: center;
+`;
+
+const DemoArea = styled(Col)`
+  width: 100%;
+  height: 85vh;
+`;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContainerDiv fluid>
+      <Row>
+        <Col>
+          <h1>Hello CodeSandbox</h1>
+        </Col>
+      </Row>
+      <Row>
+        <DemoArea>
+          <OverviewFlow />
+        </DemoArea>
+      </Row>
+    </ContainerDiv>
   );
 }
-
-export default App;
